e5c13a04e3fa8ca3368179bf66faec15730e009e
Chas Leichner <charlesl@email.arizona.edu>
2011-12-06 07:12:45 +0000

    Added command line flags to control ports.

--- pipeclock.py
+++ pipeclock.py
@@ -4,15 +4,24 @@ reads from a pull socket and times how long each request takes, ignores what it
 receives
 '''
 
+import argparse
 import os
 import time
 import zmq
 
-port = os.environ.get('PORT', '5558')
+parser = argparse.ArgumentParser(
+    description='Reads from a pull socket and prints out the time that it'
+                'receives each message.')
+
+parser.add_argument('--port', dest='port', type=int, nargs='?',
+                    default=int(os.environ.get('PORT', 5558)),
+                    help='the port that pipeclock will pull from')
+
+args = parser.parse_args()
 
 context = zmq.Context()
 receiver = context.socket(zmq.PULL)
-receiver.connect("tcp://localhost:" + port)
+receiver.connect("tcp://localhost:%d" % args.port)
 
 requests = 0
 start = time.time()
--- zc.py
+++ zc.py
@@ -5,17 +5,25 @@ sends the result over a push socket on port 5558 or the port defined by the
 environmental variable PORT
 '''
 
+import argparse
 import os
 import sys
 import zmq
 
-port = os.environ.get('PORT', '5558')
+parser = argparse.ArgumentParser(
+    description='Read from stdin and send contents to a ZMQ socket')
+
+parser.add_argument('--port', dest='port', type=int, nargs='?',
+                    default=int(os.environ.get('PORT', 5558)),
+                    help='the port that zc will push over')
+
+args = parser.parse_args()
 
 context = zmq.Context()
 sender = context.socket(zmq.PUSH)
-sender.bind("tcp://*:" + port)
+sender.bind("tcp://*:%d" % args.port)
 
 while True:
-    for line in sys.stdin:
-        sender.send(line)
+    sender.send(sys.stdin.read())
+    
 
