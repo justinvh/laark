ce3d04f7bf1dad68fb0d1d391039924bac66bd08
Chas Leichner <chas@chas.io>
2011-12-06 07:38:27 +0000

    Added delta timing, simplified output

--- pipeclock.py
+++ pipeclock.py
@@ -1,7 +1,7 @@
 '''
 pipeclock --
-reads from a pull socket and times how long each request takes, ignores what it
-receives
+Reads from a pull socket and times how long each request takes, ignores what it
+receives.
 '''
 
 import argparse
@@ -10,8 +10,8 @@ import time
 import zmq
 
 parser = argparse.ArgumentParser(
-    description='Reads from a pull socket and prints out the time that it'
-                'receives each message.')
+    description='Reads from a pull socket and prints out the time that it '
+                'receives each message and how long it took to receive it.')
 
 parser.add_argument('--port', dest='port', type=int, nargs='?',
                     default=int(os.environ.get('PORT', 5558)),
@@ -23,12 +23,11 @@ context = zmq.Context()
 receiver = context.socket(zmq.PULL)
 receiver.connect("tcp://localhost:%d" % args.port)
 
-requests = 0
-start = time.time()
+start = last = time.time()
 while True:
     receiver.recv()
     current = time.time()
-    print "Request %d at time %f" % (requests, (current-start))
-    requests += 1
+    print "%f %f" % (current-start, current-last)
+    last = current
 
 
