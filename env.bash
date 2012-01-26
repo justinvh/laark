73348a79495fc68974b5af6d02aedab253f0ade1
Justin Bruce Van Horne <justinvh@email.arizona.edu>
2012-01-26 04:12:19 +0000

    Removed execution on env.bash
    - Removed bench.cc and save_image.cc
    - Added source check to env.bash

--- env.bash
+++ env.bash
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [[ "$BASH_SOURCE" == "$0" ]]
+then
+    echo usage: \$ source $0
+    exit 1
+fi
+
 LAARK_PATH="$( cd "$( dirname "$0" )" && pwd )"
 export PYTHONPATH=$PYTHONPATH:$LAARK_PATH/python
 export PATH=$LAARK_PATH/bin:$PATH
