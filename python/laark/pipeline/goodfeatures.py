5b529ccdb6db1df84d9e08847a3ea165c4614707
Chris Van Horne <chris.vanhorne@gmail.com>
2012-01-26 06:53:43 +0000

    Initial feature detection with subrect extraction.

---
+++ python/laark/pipeline/goodfeatures.py
@@ -0,0 +1,60 @@
+'''
+good-features --
+Reads a RAW BGR image via a PULL socket and uses the
+OpenCV GoodFeaturesToTrack method to find subrectangles
+to send over a PUSH socket.
+'''
+
+from cStringIO import StringIO
+from laark.decorator.pipeline import pipeline
+import argparse
+import cv
+import json
+import sys
+
+parser = argparse.ArgumentParser(
+        description='Reads a RAW BGR image and finds '
+                    'interesting subrects to forward')
+
+parser.add_argument('-n', '--num-features', type=int, default=10, nargs=1,
+        help='The number of features to extract from the incoming image')
+
+parser.add_argument('-i', '--in-port', type=int,
+        help='The incoming port to listen for RAW images')
+
+parser.add_argument('-o', '--out-port', type=int, nargs='?',
+        help='The port to PUSH subrectangles out')
+
+args = parser.parse_args()
+print 'listening on', args.in_port, 'and pushing to', args.out_port, '...'
+
+@pipeline(in_ports=args.in_port, out_port=args.out_port)
+def worker(data):
+    buf = StringIO(data)
+    raw = buf.read()
+
+    width = int(raw[:6].lstrip())
+    height = int(raw[6:12].lstrip())
+    channels = int(raw[12:13].lstrip())
+
+    meta = json.loads(raw[13+width*height*channels:])
+
+    img = cv.CreateImageHeader((width, height), cv.IPL_DEPTH_8U, channels)
+    cv.SetData(img, raw[13:width*height*channels])
+    gs = cv.CreateImage((width, height), 8, 1)
+    cv.CvtColor(img, gs, cv.CV_BGR2GRAY)
+    
+    eigimg = cv.CreateMat(gs.width, gs.height, cv.CV_8UC1)
+    tmpimg = cv.CreateMat(gs.width, gs.height, cv.CV_8UC1)
+
+    print '\nnew image'
+    for x, y in cv.GoodFeaturesToTrack(gs, eigimg, tmpimg, args.num_features, 0.02, 1.0, useHarris=True):
+        print 'good feature at', x, y
+        sub = cv.GetSubRect(img, (int(x), int(y), 128, 128))
+
+        filename = '/tmp/feature-%d-%d.png' % (x, y)
+        cv.SaveImage(filename, sub)
+
+    return "asdf"
+
+worker.run()
