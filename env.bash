bb18909401c99495a793850e12db5f3bbd16ed63
Chris Van Horne <chris.vanhorne@gmail.com>
2012-01-30 23:43:34 +0000

    Add shell script for snap (cams.)

--- env.bash
+++ env.bash
@@ -6,6 +6,6 @@ then
     exit 1
 fi
 
-LAARK_PATH="$( cd "$( dirname "$0" )" && pwd )"
-export PYTHONPATH=$PYTHONPATH:$LAARK_PATH/python
-export PATH=$LAARK_PATH/bin:$PATH
+export LAARK="$( cd "$( dirname "$0" )" && pwd )"
+export PYTHONPATH=$PYTHONPATH:$LAARK/python
+export PATH=$LAARK/bin:$PATH
